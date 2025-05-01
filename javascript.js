144f4df45df6a7e9314fea63cb0b9425814b06e4
JoonasKaarni <bhu216@myy.haaga-helia.fi>
2025-05-02 01:17:34 +0300

    koodin siivousta ja nyt voi klikata uudestaan lukittua luukkua ja sulkee itsensä

--- javascript.js
+++ javascript.js
@@ -1,10 +1,28 @@
+let tanaan = new Date();
+
+const viikot = [
+    { start: "2025-06-01", end: "2025-06-07"},
+    { start: "2025-06-08", end: "2025-06-14"},
+    { start: "2025-06-15", end: "2025-06-21"},
+    { start: "2025-06-22", end: "2025-06-28"},
+    { start: "2025-06-29", end: "2025-07-05"},
+    { start: "2025-07-06", end: "2025-07-12"},
+    { start: "2025-07-13", end: "2025-07-19"},
+    { start: "2025-07-20", end: "2025-07-26"},
+    { start: "2025-07-27", end: "2025-08-02"},
+    { start: "2025-08-03", end: "2025-08-09"},
+    { start: "2025-08-10", end: "2025-08-16"},
+    { start: "2025-08-17", end: "2025-08-23"},
+    { start: "2025-08-24", end: "2025-08-30"}
+]
 function avaaLuukku1 () {
-    let viikko = {start: "2025-04-20", end: "2025-04-26",};
-    let tanaan = new Date();
+    let viikko = viikot[0];
     let start = new Date(viikko.start);
     let end = new Date(viikko.end);
     let teksti = "Kiasmassa on perjantaina ilmainen sisäänpääsy!";
     if (document.getElementById("l1").innerHTML === "Viikko 1 on avattu!<br>" + teksti) {
+        document.getElementById("l1").innerHTML = "Viikko 1"
+    } else if (document.getElementById("l1").innerHTML === "Viikko 1 on vielä lukittu.") {
         document.getElementById("l1").innerHTML = "Viikko 1"
     } else if (tanaan >= start && tanaan <= end) {
         document.getElementById("l1").innerHTML = "Viikko 1 on avattu!<br>" + teksti;
@@ -16,12 +34,13 @@ function avaaLuukku1 () {
 }
 
 function avaaLuukku2 () {
-    let viikko = {start: "2025-04-20", end: "2025-04-26",};
-    let tanaan = new Date();
+    let viikko = viikot[1];
     let start = new Date(viikko.start);
     let end = new Date(viikko.end);
     let teksti = "Luonnontieteellisessä museossa on ilta museossa -tapahtuma!";
     if (document.getElementById("l2").innerHTML === "Viikko 2 on avattu!<br>" + teksti) {
+        document.getElementById("l2").innerHTML = "Viikko 2"
+    } else if (document.getElementById("l2").innerHTML === "Viikko 2 on vielä lukittu.") {
         document.getElementById("l2").innerHTML = "Viikko 2"
     } else if (tanaan >= start && tanaan <= end) {
         document.getElementById("l2").innerHTML = "Viikko 2 on avattu!<br>" + teksti;
@@ -32,12 +51,13 @@ function avaaLuukku2 () {
     }
 }
 function avaaLuukku3 () {
-    let viikko = {start: "2025-04-20", end: "2025-04-26",};
-    let tanaan = new Date();
+    let viikko = viikot[2];
     let start = new Date(viikko.start);
     let end = new Date(viikko.end);
     let teksti = "Juhannusviikko. Seurasaaressa on ilmainen tapahtuma kokkoineen!";
     if (document.getElementById("l3").innerHTML === "Viikko 3 on avattu!<br>" + teksti) {
+        document.getElementById("l3").innerHTML = "Viikko 3"
+    } else if (document.getElementById("l3").innerHTML === "Viikko 3 on vielä lukittu.") {
         document.getElementById("l3").innerHTML = "Viikko 3"
     } else if (tanaan >= start && tanaan <= end) {
         document.getElementById("l3").innerHTML = "Viikko 3 on avattu!<br>" + teksti;
@@ -48,12 +68,13 @@ function avaaLuukku3 () {
     }
 }
 function avaaLuukku4 () {
-    let viikko = {start: "2025-04-20", end: "2025-04-26",};
-    let tanaan = new Date();
+    let viikko = viikot[3];
     let start = new Date(viikko.start);
     let end = new Date(viikko.end);
     let teksti = "Helsingin keskustassa juhlitaan Tuska festivaalia, sekä prideä!";
     if (document.getElementById("l4").innerHTML === "Viikko 4 on avattu!<br>" + teksti) {
+        document.getElementById("l4").innerHTML = "Viikko 4"
+    } else if (document.getElementById("l4").innerHTML === "Viikko 4 on vielä lukittu.") {
         document.getElementById("l4").innerHTML = "Viikko 4"
     } else if (tanaan >= start && tanaan <= end) {
         document.getElementById("l4").innerHTML = "Viikko 4 on avattu!<br>" + teksti;
@@ -64,12 +85,13 @@ function avaaLuukku4 () {
     }
 }
 function avaaLuukku5 () {
-    let viikko = {start: "2025-04-20", end: "2025-04-26",};
-    let tanaan = new Date();
+    let viikko = viikot[4];
     let start = new Date(viikko.start);
     let end = new Date(viikko.end);
     let teksti = "Heurekassa on jääkauden jättiläiset -näyttely!";
     if (document.getElementById("l5").innerHTML === "Viikko 5 on avattu!<br>" + teksti) {
+        document.getElementById("l5").innerHTML = "Viikko 5"
+    } else if (document.getElementById("l5").innerHTML === "Viikko 5 on vielä lukittu.") {
         document.getElementById("l5").innerHTML = "Viikko 5"
     } else if (tanaan >= start && tanaan <= end) {
         document.getElementById("l5").innerHTML = "Viikko 5 on avattu!<br>" + teksti;
@@ -80,12 +102,13 @@ function avaaLuukku5 () {
     }
 }
 function avaaLuukku6 () {
-    let viikko = {start: "2025-04-20", end: "2025-04-26",};
-    let tanaan = new Date();
+    let viikko = viikot[5];
     let start = new Date(viikko.start);
     let end = new Date(viikko.end);
     let teksti = "Helsinki Cup, tämä jalkapallo tapahtuma kestää koko viikon!";
     if (document.getElementById("l6").innerHTML === "Viikko 6 on avattu!<br>" + teksti) {
+        document.getElementById("l6").innerHTML = "Viikko 6"
+    } else if (document.getElementById("l6").innerHTML === "Viikko 6 on vielä lukittu.") {
         document.getElementById("l6").innerHTML = "Viikko 6"
     } else if (tanaan >= start && tanaan <= end) {
         document.getElementById("l6").innerHTML = "Viikko 6 on avattu!<br>" + teksti;
@@ -96,12 +119,13 @@ function avaaLuukku6 () {
     }
 }
 function avaaLuukku7 () {
-    let viikko = {start: "2025-04-20", end: "2025-04-26",};
-    let tanaan = new Date();
+    let viikko = viikot[6];
     let start = new Date(viikko.start);
     let end = new Date(viikko.end);
     let teksti = "Olympiapäivä Helsingin olympiastadionilla, tapahtuma on ilmainen!";
     if (document.getElementById("l7").innerHTML === "Viikko 7 on avattu!<br>" + teksti) {
+        document.getElementById("l7").innerHTML = "Viikko 7"
+    } else if (document.getElementById("l7").innerHTML === "Viikko 7 on vielä lukittu.") {
         document.getElementById("l7").innerHTML = "Viikko 7"
     } else if (tanaan >= start && tanaan <= end) {
         document.getElementById("l7").innerHTML = "Viikko 7 on avattu!<br>" + teksti;
@@ -112,12 +136,13 @@ function avaaLuukku7 () {
     }
 }
 function avaaLuukku8 () {
-    let viikko = {start: "2025-04-20", end: "2025-04-26",};
-    let tanaan = new Date();
+    let viikko = viikot[7];
     let start = new Date(viikko.start);
     let end = new Date(viikko.end);
     let teksti = "Suomenlinnan kesäteatterissa on tarjolla runsaasti näytelmiä!";
     if (document.getElementById("l8").innerHTML === "Viikko 8 on avattu!<br>" + teksti) {
+        document.getElementById("l8").innerHTML = "Viikko 8"
+    } else if (document.getElementById("l8").innerHTML === "Viikko 8 on vielä lukittu.") {
         document.getElementById("l8").innerHTML = "Viikko 8"
     } else if (tanaan >= start && tanaan <= end) {
         document.getElementById("l8").innerHTML = "Viikko 8 on avattu!<br>" + teksti;
@@ -128,12 +153,13 @@ function avaaLuukku8 () {
     }
 }
 function avaaLuukku9 () {
-    let viikko = {start: "2025-04-20", end: "2025-04-26",};
-    let tanaan = new Date();
+    let viikko = viikot[8];
     let start = new Date(viikko.start);
     let end = new Date(viikko.end);
     let teksti = "Päiväreissu Linnanmäelle, sisäänpääsy on ilmainen!";
     if (document.getElementById("l9").innerHTML === "Viikko 9 on avattu!<br>" + teksti) {
+        document.getElementById("l9").innerHTML = "Viikko 9"
+    } else if (document.getElementById("l9").innerHTML === "Viikko 9 on vielä lukittu.") {
         document.getElementById("l9").innerHTML = "Viikko 9"
     } else if (tanaan >= start && tanaan <= end) {
         document.getElementById("l9").innerHTML = "Viikko 9 on avattu!<br>" + teksti;
@@ -144,12 +170,13 @@ function avaaLuukku9 () {
     }
 }
 function avaaLuukku10 () {
-    let viikko = {start: "2025-04-20", end: "2025-04-26",};
-    let tanaan = new Date();
+    let viikko = viikot[9];
     let start = new Date(viikko.start);
     let end = new Date(viikko.end);
     let teksti = "Helsingissä juhlitaan Flow festivaaleja!";
     if (document.getElementById("l10").innerHTML === "Viikko 10 on avattu!<br>" + teksti) {
+        document.getElementById("l10").innerHTML = "Viikko 10"
+    } else if (document.getElementById("l10").innerHTML === "Viikko 10 on vielä lukittu.") {
         document.getElementById("l10").innerHTML = "Viikko 10"
     } else if (tanaan >= start && tanaan <= end) {
         document.getElementById("l10").innerHTML = "Viikko 10 on avattu!<br>" + teksti;
@@ -160,12 +187,13 @@ function avaaLuukku10 () {
     }
 }
 function avaaLuukku11 () {
-    let viikko = {start: "2025-04-20", end: "2025-04-26",};
-    let tanaan = new Date();
+    let viikko = viikot[10];
     let start = new Date(viikko.start);
     let end = new Date(viikko.end);
     let teksti = "Päiväreissu Fallkullan kotieläintilalle!";
     if (document.getElementById("l11").innerHTML === "Viikko 11 on avattu!<br>" + teksti) {
+        document.getElementById("l11").innerHTML = "Viikko 11"
+    } else if (document.getElementById("l11").innerHTML === "Viikko 11 on vielä lukittu.") {
         document.getElementById("l11").innerHTML = "Viikko 11"
     } else if (tanaan >= start && tanaan <= end) {
         document.getElementById("l11").innerHTML = "Viikko 11 on avattu!<br>" + teksti;
@@ -176,12 +204,13 @@ function avaaLuukku11 () {
     }
 }
 function avaaLuukku12 () {
-    let viikko = {start: "2025-04-20", end: "2025-04-26",};
-    let tanaan = new Date();
+    let viikko = viikot[11];
     let start = new Date(viikko.start);
     let end = new Date(viikko.end);
     let teksti = "Smashing Pumpkins esiintyy Helsingin jäähallissa!";
     if (document.getElementById("l12").innerHTML === "Viikko 12 on avattu!<br>" + teksti) {
+        document.getElementById("l12").innerHTML = "Viikko 12"
+    } else if (document.getElementById("l12").innerHTML === "Viikko 12 on vielä lukittu.") {
         document.getElementById("l12").innerHTML = "Viikko 12"
     } else if (tanaan >= start && tanaan <= end) {
         document.getElementById("l12").innerHTML = "Viikko 12 on avattu!<br>" + teksti;
@@ -192,12 +221,13 @@ function avaaLuukku12 () {
     }
 }
 function avaaLuukku13 () {
-    let viikko = {start: "2025-04-20", end: "2025-04-26",};
-    let tanaan = new Date();
+    let viikko = viikot[12];
     let start = new Date(viikko.start);
     let end = new Date(viikko.end);
     let teksti = "Venetsialaiset, eli mökkikauden päätösjuhlat. Runsaasti ilotulituksia ympäri pääkaupunkiseutua!";
     if (document.getElementById("l13").innerHTML === "Viikko 13 on avattu!<br>" + teksti) {
+        document.getElementById("l13").innerHTML = "Viikko 13"
+    } else if (document.getElementById("l13").innerHTML === "Viikko 13 on vielä lukittu.") {
         document.getElementById("l13").innerHTML = "Viikko 13"
     } else if (tanaan >= start && tanaan <= end) {
         document.getElementById("l13").innerHTML = "Viikko 13 on avattu!<br>" + teksti;
@@ -207,17 +237,3 @@ function avaaLuukku13 () {
         document.getElementById("l13").innerHTML = "Viikko 13 on vielä lukittu.";
     }
 }
-
-      /* Tässä on viikot 2-13 oikeat päivämäärät. ne voi kopsaa tästä noihin sit kun niihin laittaa ne oikeat päivämäärät.
-      { start: "2024-06-08", end: "2024-06-14", message: "Tähän tulee toisen viikon tapahtumat" },
-        { start: "2024-06-15", end: "2024-06-21", message: "Tähän tulee kolmannen viikon tapahtumat" },
-        { start: "2024-06-22", end: "2024-06-28", message: "Tähän tulee neljännen viikon tapahtumat" },
-        { start: "2024-06-29", end: "2024-07-05", message: "Tähän tulee viidennen viikon tapahtumat" },
-        { start: "2024-07-06", end: "2024-07-12", message: "Tähän tulee kuudennen viikon tapahtumat" },
-        { start: "2024-07-13", end: "2024-07-19", message: "Tähän tulee seitsemännen viikon tapahtumat" },
-        { start: "2024-07-20", end: "2024-07-26", message: "Tähän tulee kahdeksannen viikon tapahtumat" },
-        { start: "2024-07-27", end: "2024-08-02", message: "Tähän tulee yhdeksännen viikon tapahtumat" },
-        { start: "2024-08-03", end: "2024-08-09", message: "Tähän tulee kymmenennen viikon tapahtumat" },
-        { start: "2024-08-10", end: "2024-08-16", message: "Tähän tulee yhdennentoista viikon tapahtumat" },
-        { start: "2024-08-17", end: "2024-08-23", message: "Tähän tulee kahdennentoista viikon tapahtumat" },
-        { start: "2024-08-24", end: "2024-08-30", message: "Tähän tulee kolmannentoista viikon tapahtumat" }*/
